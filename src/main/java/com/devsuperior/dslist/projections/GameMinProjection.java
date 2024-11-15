95c389784c9503315da36cd63fa08e964e973b66
josymartns <25josy25@gmail.com>
2024-11-14 22:16:05 -0300

    Projection

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUml();
+	String getShortDescription();
+	Integer getPosition(); 
+	
+}
